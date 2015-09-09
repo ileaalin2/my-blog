b7d38515ba81d0f01a933df2e4fd2081b001f4bb
ileaalin2 <ilea.alin2@gmail.com>
2015-09-09 13:27:56 +0300

    Create contact.php

---
+++ views/contact.php
@@ -0,0 +1,41 @@
+<!-- main content -->
+<main class="main-contact">
+    <div>
+		<h2>Contact</h2>
+		<div class="flag">
+    		<div><a href="https://www.facebook.com/ilearadualin" target="_blank">
+    		<img src="public/img/logo/facebook.ico" alt="Facebook logo" title="Click to access my Facebook page"
+    		style="width:40px; height:40px; border: 0;" /></a></div>
+    		<div><a href="https://ro.linkedin.com/in/ilearadualin" target="_blank">
+    		<img src="public/img/logo/linkedin.png" alt="Linkedin logo" title="Click to access my Linkedin page"
+    		style="width:40px; height:40px; border: 0;" /></a></div>
+    		<div><img src="public/img/logo/skype.png" alt="Skype logo" style="width:40px; height:40px; border:0"
+    		title="My Skype ID is 'alinilea'" /></div>
+        </div>
+<!--        <p></p>
+		    <br>
+            <hr style="width: 90%;">
+        	<br>  
+    		<p>If you want to send me a message, please fill in the form bellow and ... <br/>
+    		click on the <q>Send</q> button when you're done.</p>
+    		<p>I will respond to you as soon as I can. Thank you!</p>
+    		<p>Note that only phone number is optional!</p>
+            <br />
+-->
+        <p>If you want to send me a message, please fill in the form bellow!</p>
+        <form action="" method="POST">
+			<p><label>&#9830; &#160; First name:</label>
+			<input type="text" name="firstname" size="30" maxlength="25" required /></p>
+			<p><label>&#9830; &#160; Last name:</label>
+			<input type="text" name="lastname" size="30" maxlength="25" required /></p>
+			<p><label>&#9830; &#160; E-mail address:</label>
+			<input type="text" name="emailaddress" size="30" maxlength="50" required /></p>
+			<p><label>&#9830; &#160; &#x260F; number (optional):</label>
+			<input type="text" name="phone" size="30" maxlength="25" /></p>
+			<p></p>
+			<p><textarea cols="55" rows="5" name="message" maxlength="1000" placeholder="&#9830; Type your message here" wrap="hard" required /></textarea></p>
+			<p><input type="submit" name="submit" value="Send" style="font-family: times; font-size: 20px; width: 60px; background-color: #5C9DDE" />
+			<input type="reset" name="reset" value="Reset" style="font-family: times; font-size: 20px; width: 60px; background-color: #5C9DDE" /></p>
+		</form>
+	</div>
+</main>

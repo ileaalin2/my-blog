f9f0616f3c5ec922a0e6b54426709cd21bb7adb1
ileaalin2 <ilea.alin2@gmail.com>
2015-09-09 13:26:33 +0300

    Create hobby.php

---
+++ views/hobby.php
@@ -0,0 +1,170 @@
+<!-- main content -->
+<main>
+    <div>
+        <h2>My hobbies</h2>
+<!--                   <p><strong><i>Here it'll be text about</i></strong> <mark>my hobbies</mark></p>  -->
+       <ul class="menu-hobby">
+            <li><a href="#bowen">&#9654; bowtech</a></li>
+            <li><a href="#hiking">&#9654; hiking</a></li>
+            <li><a href="#reading">&#9654; reading</a></li>
+            <li><a href="#music" title="Listening music">&#9654; listening &#x266C;</a></li>
+            <li><a href="#movie">&#9654; watching movies</a></li>
+            <li><a href="#football" title="Playing Football">&#9654; playing &#x26BD;</a></li>
+            <li><a href="#basketball" title="Playing Basketball">&#9654; playing &#x26BE;</a></li>
+            <li><a href="#dance">&#9654; folkloric dancing</a></li>
+            <li><a href="#jogging">&#9654; jogging</a></li>
+            <li><a href="#swimming">&#9654; swimming</a></li>
+            <li><a href="#cooking">&#9654; cooking</a></li>
+       </ul>
+    </div>
+    <div id="bgbowen" title="Bowtech" style="margin-top: 50px; width: 900px; height: 490px;">
+        <h3 id="bowen">The Bowen Technique</h3>
+        <img src="public/img/pozebowen/lessons2.jpg" alt="A Bowen move" title="A Bowen move"
+        style="float: right; margin-left: 20px; width: 250px; height: 170px; border: 1px;">
+        <p>From alternative medicine techniques I chose 
+        <strong><em>The Bowen Therapy Technique</em></strong> also known as Bowtech, which is a dynamic system 
+        of muscle and connective tissue therapy that was developed by Tom Bowen in Australia.
+        This technique can provide relief for many types of injuries and other health problems,
+        both acute and chronic, and it does so holistically, via the body's innate healing mechanisms.</p>
+        <p>While there are a few, very specific situations in which a particular 'Bowen procedure' is
+        contraindicated, Bowen Technique is appropriate for people of all ages, in all degrees of health.
+        Rather than focusing on a single complaint, Bowen Technique addresses the entire body,
+        by restoring balance via the autonomic nervous system (ANS).</p> 
+        <p>The ANS controls over 80% of bodily functions 
+        and is very susceptible to external stressors.
+        Most people today live in a constant state of high stress and sympathetic ANS over-stimulation
+        (fight, run or freeze mode). Healing can occur only after the ANS shifts from sympathetic to
+        parasympathetic dominance (rest, relax and repair mode). Bowen Technique enables that shift.
+        During a session, the client often drops into deep relaxation. That change is the indicator of a 
+        profound release from stress and a shift towards parasympathetic influence.</p>
+        <p>You can find more information here:
+        <a href="http://www.bowtech.ro" target="_blank"><img src="public/img/pozebowen/bowtech-slogo.png" alt="Bowen logo"
+        title="www.bowtech.ro" style="margin-left: 380px; width: 436px; height: 110px; border: 0"></a></p>
+    </div>
+    <div>
+        <h3 id="hiking">Hiking</h3>
+        <p>If you want happiness for a lifetime help someone else! If you want happiness for a lifetime help someone else!
+        If you want happiness for a lifetime help someone else! If you want happiness for a lifetime help someone else!
+        If you want happiness for a lifetime help someone else! If you want happiness for a lifetime help someone else!
+        If you want happiness for a lifetime help someone else! If you want happiness for a lifetime help someone else!
+        If you want happiness for a lifetime help someone else! If you want happiness for a lifetime help someone else!
+        If you want happiness for a lifetime help someone else! If you want happiness for a lifetime help someone else!</p>
+    </div>
+    <div>
+       <h3 id="reading">Reading</h3>
+        <p>"Perseverance is a virtue of the less brilliant" Santiago Ramo&#769;n y Cajal.
+        "Perseverance is a virtue of the less brilliant" Santiago Ramo&#769;n y Cajal.
+        "Perseverance is a virtue of the less brilliant" Santiago Ramo&#769;n y Cajal.
+        "Perseverance is a virtue of the less brilliant" Santiago Ramo&#769;n y Cajal.
+        "Perseverance is a virtue of the less brilliant" Santiago Ramo&#769;n y Cajal.
+        "Perseverance is a virtue of the less brilliant" Santiago Ramo&#769;n y Cajal.
+        "Perseverance is a virtue of the less brilliant" Santiago Ramo&#769;n y Cajal.
+        "Perseverance is a virtue of the less brilliant" Santiago Ramo&#769;n y Cajal.
+        "Perseverance is a virtue of the less brilliant" Santiago Ramo&#769;n y Cajal.</p>
+    </div>
+    <div>
+       <h3 id="music" title="Listening music">Listening  &#x266C;</h3>
+        <p>A DataBase Administrator walks into a bar. Sees two tables in the corner, walks up to them, and says: "May I JOIN you?". 
+        A DataBase Administrator walks into a bar. Sees two tables in the corner, walks up to them, and says: "May I JOIN you?". 
+        A DataBase Administrator walks into a bar. Sees two tables in the corner, walks up to them, and says: "May I JOIN you?". 
+        A DataBase Administrator walks into a bar. Sees two tables in the corner, walks up to them, and says: "May I JOIN you?". 
+        A DataBase Administrator walks into a bar. Sees two tables in the corner, walks up to them, and says: "May I JOIN you?". 
+        A DataBase Administrator walks into a bar. Sees two tables in the corner, walks up to them, and says: "May I JOIN you?". 
+        A DataBase Administrator walks into a bar. Sees two tables in the corner, walks up to them, and says: "May I JOIN you?". 
+        A DataBase Administrator walks into a bar. Sees two tables in the corner, walks up to them, and says: "May I JOIN you?". 
+        A DataBase Administrator walks into a bar. Sees two tables in the corner, walks up to them, and says: "May I JOIN you?".</p> 
+    </div>
+    <div>
+       <h3 id="movie">Watching movies</h3>
+        <p>The most important thing in life is not knowing everything, it's having the phone number of somebody who does! 
+        The most important thing in life is not knowing everything, it's having the phone number of somebody who does! 
+        The most important thing in life is not knowing everything, it's having the phone number of somebody who does! 
+        The most important thing in life is not knowing everything, it's having the phone number of somebody who does! 
+        The most important thing in life is not knowing everything, it's having the phone number of somebody who does! 
+        The most important thing in life is not knowing everything, it's having the phone number of somebody who does! 
+        The most important thing in life is not knowing everything, it's having the phone number of somebody who does! 
+        The most important thing in life is not knowing everything, it's having the phone number of somebody who does! 
+        The most important thing in life is not knowing everything, it's having the phone number of somebody who does!</p>
+    </div>
+    <div>
+       <h3 id="football" title="Playing Football">Playing &#x26BD;</h3>
+        <p>I changed my password everywhere to 'incorrect.' 
+        That way when I forget it, it always reminds me, 'Your password is incorrect.' 
+        I changed my password everywhere to 'incorrect.' 
+        That way when I forget it, it always reminds me, 'Your password is incorrect.' 
+        I changed my password everywhere to 'incorrect.' 
+        That way when I forget it, it always reminds me, 'Your password is incorrect.' 
+        I changed my password everywhere to 'incorrect.' 
+        That way when I forget it, it always reminds me, 'Your password is incorrect.' 
+        I changed my password everywhere to 'incorrect.' 
+        That way when I forget it, it always reminds me, 'Your password is incorrect.' 
+        I changed my password everywhere to 'incorrect.' 
+        That way when I forget it, it always reminds me, 'Your password is incorrect.' 
+        I changed my password everywhere to 'incorrect.' 
+        That way when I forget it, it always reminds me, 'Your password is incorrect.'</p>
+    </div>
+    <div>
+       <h3 id="basketball" title="Playing Basketball">Playing &#x26BE;</h3>
+        <p>If you think patience is a virtue, try surfing the net without high speed Internet. 
+        If you think patience is a virtue, try surfing the net without high speed Internet. 
+        If you think patience is a virtue, try surfing the net without high speed Internet. 
+        If you think patience is a virtue, try surfing the net without high speed Internet. 
+        If you think patience is a virtue, try surfing the net without high speed Internet. 
+        If you think patience is a virtue, try surfing the net without high speed Internet. 
+        If you think patience is a virtue, try surfing the net without high speed Internet.</p> 
+    </div>
+    <div>
+       <h3 id="dance">Folkloric dancing</h3>
+        <p>Don't worry if plan A fails, there are 25 more letters in the alphabet. 
+        Don't worry if plan A fails, there are 25 more letters in the alphabet. 
+        Don't worry if plan A fails, there are 25 more letters in the alphabet. 
+        Don't worry if plan A fails, there are 25 more letters in the alphabet. 
+        Don't worry if plan A fails, there are 25 more letters in the alphabet. 
+        Don't worry if plan A fails, there are 25 more letters in the alphabet. 
+        Don't worry if plan A fails, there are 25 more letters in the alphabet.</p> 
+    </div>
+    <div>
+       <h3 id="jogging">Jogging</h3>
+        <p>You love flowers, but you cut them. You love animals, but you eat them. 
+        You tell me you love me, so now I'm scared! 
+        You love flowers, but you cut them. You love animals, but you eat them. 
+        You tell me you love me, so now I'm scared! 
+        You love flowers, but you cut them. You love animals, but you eat them. 
+        You tell me you love me, so now I'm scared! 
+        You love flowers, but you cut them. You love animals, but you eat them. 
+        You tell me you love me, so now I'm scared! 
+        You love flowers, but you cut them. You love animals, but you eat them. 
+        You tell me you love me, so now I'm scared! 
+        You love flowers, but you cut them. You love animals, but you eat them. 
+        You tell me you love me, so now I'm scared! 
+        You love flowers, but you cut them. You love animals, but you eat them. 
+        You tell me you love me, so now I'm scared!</p> 
+    </div>
+    <div>
+       <h3 id="swimming">Swimming</h3>
+        <p>Everyone has photographic memory; some just don't have the film. 
+        Everyone has photographic memory; some just don't have the film. 
+        Everyone has photographic memory; some just don't have the film. 
+        Everyone has photographic memory; some just don't have the film. 
+        Everyone has photographic memory; some just don't have the film. 
+        Everyone has photographic memory; some just don't have the film. 
+        Everyone has photographic memory; some just don't have the film.</p> 
+    </div>
+    <div>
+        <h3 id="cooking">Cooking</h3>
+        <p>Some of the greatest ideas of all time have come to people during Math class... 
+        none of which had anything to do with Math. 
+        Some of the greatest ideas of all time have come to people during Math class... 
+        none of which had anything to do with Math. 
+        Some of the greatest ideas of all time have come to people during Math class... 
+        none of which had anything to do with Math. 
+        Some of the greatest ideas of all time have come to people during Math class... 
+        none of which had anything to do with Math. 
+        Some of the greatest ideas of all time have come to people during Math class... 
+        none of which had anything to do with Math. 
+        Some of the greatest ideas of all time have come to people during Math class... 
+        none of which had anything to do with Math. 
+        Some of the greatest ideas of all time have come to people during Math class... 
+        none of which had anything to do with Math.</p>
+    </div>
+</main>

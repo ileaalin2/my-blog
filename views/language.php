be23c65784024b1edefc66a193753c73792981be
ileaalin2 <ilea.alin2@gmail.com>
2015-09-09 13:27:22 +0300

    Create language.php

---
+++ views/language.php
@@ -0,0 +1,109 @@
+<!-- main content -->
+<main>
+    <div>
+        <h2>Languages</h2>
+        <p><strong><i>Here it'll be text about</i></strong> <mark>the languages</mark> I know.</p>
+       <ul class="menu-language">
+            <li><a href="#english">English</a></li>
+            <li><a href="#german">German</a></li>
+            <li><a href="#french">French</a></li>
+       </ul>
+    </div>
+    <div title="The English Language">
+        <img src="public/img/language/enter-en.jpg" alt="English Enter" title="English Enter"
+        style="float: right; margin-right: 50px; width: 250px; height: auto; border: 1px;"></p>
+        <h3 id="english">The English Language</h3>
+        <p>In this section I write about:
+        <h3 style="text-indent: 50px;">Deep English</h3>
+<!--                    <p>They helped me a lot to improve my listening and speaking fluency in English.<br></p>
+        <p>Thank you!<br/></p> 
+-->
+        <p>Deep English is founded by Dan and Aaron. Here's what they say about themselves and about their project.<br/></p>
+        <p> With Master's degrees in teaching English and online learning, and years of experience teaching English at the 
+        university level, we are confident in our abilities to help you take your 
+        English speaking and listening to the next level.</p>
+        <h4>Real World English</h4>
+        <p>Over the years we have seen three main problems for English learners: 
+        boring materials, ineffective learning methods, and lack of time to attend classes. 
+        We decided to make something different.
+        We wanted to create something new and meaningful for people like us. 
+        People who love to learn, love to laugh, and are always looking to experience 
+        the world with fresh eyes.
+        Our mission is to get you passionate about using English to communicate and understand 
+        real, meaningful ideas. To do that, we created a different kind of English lesson, built around 
+        interesting subjects that make people think deeply.</p>
+        <h4>Vision</h4>
+        <p>We are here to make a difference. It may sound "corny" but we believe that 
+        every person has an opportunity to make the world a better place. 
+        Sending ripples of positivity into the world is something we all have a chance
+         to do regardless of what kind of job we have. We are English teachers, so we try 
+        to do this by making lessons that inspire people to grow, live life more fully, 
+        and think about the world in new ways.</p>
+        <h4>Mission</h4>
+        <p>Our mission is to help you reach your English fluency goals. To do that, 
+        we have to give you a shot of raw honesty: Deep English isn't for everyone. 
+        We have created our lessons for people who are open-minded and 
+        ready to make the jump to English fluency. This isn't for beginners.
+        Are Deep English lessons right for you? There is only one way to find out. 
+        Over the past 4 years we have created a library of free listening lessons 
+        and a free 7-day course teaching our method of how to improve 
+        your listening and speaking fluency.<br/></p>
+        <h4>Here is a piece of their work:</h4>
+        <p>Lesson: "Extensive reading for better fluency"<br/></p>
+    
+        <p>Slow speed &nbsp;
+        <audio controls style="vertical-align: middle;">
+            <source src="public/audio/extensivereading-slow.mp3" type="audio/mp3">
+            Sorry! Your browser does not support the audio element.
+        </audio>
+        
+        Normal speed &nbsp;
+        <audio controls style="vertical-align: middle;">
+            <source src="public/audio/extensivereading-normal.mp3" type="audio/mp3">
+            Sorry! Your browser does not support the audio element.
+        </audio></p>
+    </div>
+    <div title="The German Language">
+        <h3 id="german">The German Language</h3>
+        <img src="public/img/language/deutsch1.jpg" alt="Deutsch" title="Deutsch"
+        style="float: right; margin-left: 20px; width: 250px; height: 170px; border: 1px;">
+        <p>I changed my password everywhere to 'incorrect.' 
+        That way when I forget it, it always reminds me, 'Your password is incorrect.'  
+        I changed my password everywhere to 'incorrect.' 
+        That way when I forget it, it always reminds me, 'Your password is incorrect.' 
+        I changed my password everywhere to 'incorrect.' 
+        That way when I forget it, it always reminds me, 'Your password is incorrect.'  
+        I changed my password everywhere to 'incorrect.'
+        That way when I forget it, it always reminds me, 'Your password is incorrect.'</p>  
+        <p>I changed my password everywhere to 'incorrect.' 
+        That way when I forget it, it always reminds me, 'Your password is incorrect.'  
+        I changed my password everywhere to 'incorrect.' 
+        That way when I forget it, it always reminds me, 'Your password is incorrect.'</p>
+        <p>Here is a short film about the German Language compared to other Languages:</p>
+        <iframe width="640" height="390" src="https://www.youtube.com/embed/ZlATOHGj9EY" 
+        frameborder="0" allowfullscreen></iframe>
+    </div>
+    <div title="The French Language">
+       <h3 id="french">The French Language</h3>
+       <img src="public/img/language/pv-francais.jpg" alt="Parlez-vous Francais?" title="Parlez-vous Francais?"
+        style="float: right; margin-left: 20px; width: 250px; height: 170px; border: 1px;">
+        <p>You love flowers, but you cut them. You love animals, but you eat them. 
+        You tell me you love me, so now I'm scared! 
+        You love flowers, but you cut them. You love animals, but you eat them. 
+        You tell me you love me, so now I'm scared! 
+        You love flowers, but you cut them. You love animals, but you eat them. 
+        You tell me you love me, so now I'm scared! 
+        You love flowers, but you cut them. You love animals, but you eat them. 
+        You tell me you love me, so now I'm scared! 
+        You love flowers, but you cut them. You love animals, but you eat them. 
+        You tell me you love me, so now I'm scared! 
+        You love flowers, but you cut them. You love animals, but you eat them. 
+        You tell me you love me, so now I'm scared! 
+        You love flowers, but you cut them. You love animals, but you eat them. 
+        You tell me you love me, so now I'm scared!</p>
+        <p>Maybe you think English is an easy to learn language. For some people, 
+        like this French gentleman, it isn't at all!</p>
+        <iframe width="640" height="390" src="https://www.youtube.com/embed/dphayJDCzog" 
+        frameborder="1" allowfullscreen></iframe>
+    </div>
+</main>

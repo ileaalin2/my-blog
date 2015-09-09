1668b2f45c79240939eb43c5568505e8eb17e46e
ileaalin2 <ilea.alin2@gmail.com>
2015-09-09 13:30:24 +0300

    Create admin.php

---
+++ views/admin.php
@@ -0,0 +1,16 @@
+<!-- main content -->
+<main class="main-contact">
+    <div>
+		<h2>Admin</h2>
+        <a href="index.php?page=login&logout=1" style="float: right;">Logout</a>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+	</div>
+</main>

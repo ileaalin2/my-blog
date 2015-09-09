b368c0c2fc99d66d9db787287519aafd2e8d881d
ileaalin2 <ilea.alin2@gmail.com>
2015-09-09 12:59:43 +0300

    Create language.php

---
+++ controllers/language.php
@@ -0,0 +1,18 @@
+<?php
+require_once "base.php";
+
+class Language extends Base {
+    function __construct() {
+        // $data['condition'] = (isset($_SESSION['logged']) && $_SESSION['logged'] ===  TRUE);
+        // $data['logged'] = "You are logged in!";
+        // $data['unlogged'] = "";
+        $data['logged'] = (isset($_SESSION['logged']) && $_SESSION['logged'] ===  TRUE) ? "You are logged in!" : "";
+
+        $data['title'] = "LanguagePage";
+        $this->render('views/top.php', $data);
+        $this->render('views/menu.php', $data);
+        $this->render('views/language.php');
+        $this->render('views/bottom.php', $data);
+    }
+}
+?>

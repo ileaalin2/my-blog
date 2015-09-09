100fde36238590a0e5273cf96cd9b380e7496737
ileaalin2 <ilea.alin2@gmail.com>
2015-09-09 13:16:09 +0300

    Create top.php

---
+++ views/top.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+	<!-- head -->
+	<head>
+		<link rel="stylesheet" type="text/css" href="public/css/normalize.css">
+		<link rel="stylesheet" type="text/css" href="public/css/style.css">
+		<link rel="icon" type="image/ico" href="public/img/logo/favicon.ico">
+		<title>{{title}}</title>
+	</head>
+	<!-- body -->
+	<body>
+		<div class="container">
+			<header class="row">
+				<div class="col-12">
+				<h2>Welcome to Alin Ilea's Blog! Enjoy it!</h2>
+				</div>
+			</header>
+
+	
+<!-- 	{{if condition}}
+ 	    {{yes}}  
+ 	{{else}}
+ 	    {{no}}
+ 	{{endif}}
+ 	
+ 	{{if condition}} {{logged}} {{else}} {{unlogged}} {{endif}}
+ 	
+				navigation bar
+			<nav class="row">
+				<div class="col-12">
+					<ul class="nav_bar">
+                        <li><a href="?page=home">Home</a></li>
+                        <li><a href="?page=hobby">Hobbies</a></li>
+                        <li><a href="?page=aboutme">About me</a></li>
+                        <li><a href="?page=contact">Contact</a></li>
+					</ul>
+				</div>
+			</nav>
+			-->

c0a23e93ad5d2ff7eca17b7eb5ae8d1b75ac4a1e
ileaalin2 <ilea.alin2@gmail.com>
2015-09-09 13:25:58 +0300

    Create home.php

---
+++ views/home.php
@@ -0,0 +1,19 @@
+<!-- main content -->
+<main>
+    <div>
+        <img src="public/img/pozealin/alin1.jpeg"  alt="Here is Alin Ilea's picture" title="Alin Ilea"
+        style="margin-right: 30px; margin-top: 100px; width: 300px; height: 300px; border: 1px solid silver;" align="right">
+    </div>
+    <div style="width: 550px; height: 400px">
+        <h2>Here it'll be some text about me, but for the moment IT'S ALL ABOUT HTML.</h2>
+        <p>HyperText Markup Language (HTML) is used for creating and visually representing a webpage.</p>
+        <p>HTML adds "markup" to standard English text. "Hyper Text" refers to links that connect Web pages 
+        to one another, making the World Wide Web what it is today. By creating and uploading Web pages 
+        to the Internet, you become an active participant in the World Wide Web. HTML supports visual images 
+        and other media as well. With the help of HTML everyone can make static as well as dynamic web sites.</p>
+        <p>HTML is the language that describes the structure and the semantic content of a web document. 
+        Content within a web page is tagged with HTML elements such as <b>&#60;img&#62;</b>, <b>&#60;title&#62;</b>, 
+        <b>&#60;p&#62;</b>, <b>&#60;div&#62;</b>, <b>&#60;picture&#62;</b>, and so forth. These elements form 
+        the building blocks of a website.</p>
+    </div>
+</main>

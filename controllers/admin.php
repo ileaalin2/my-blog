93812798352184ee69edc599614223f1f080ef10
ileaalin2 <ilea.alin2@gmail.com>
2015-09-09 13:06:32 +0300

    Create admin.php

---
+++ controllers/admin.php
@@ -0,0 +1,24 @@
+<?php
+require_once "base.php";
+
+class Admin extends Base {
+    function __construct() {
+        if(isset($_SESSION['logged']) && $_SESSION['logged']) {
+            // $data['condition'] = (isset($_SESSION['logged']) && $_SESSION['logged'] ===  TRUE);
+            // $data['logged'] = "You are logged in!";
+            // $data['unlogged'] = "";
+
+            $data['logged'] = "You are logged in!";    
+
+            $data['title'] = "AdminPage";
+            $this->render('views/top.php', $data);
+            $this->render('views/menu.php', $data);
+            $this->render('views/admin.php', $data);
+            $this->render('views/bottom.php', $data);
+        } else {
+            $data['logged'] = "";
+            header('Location: http://188.166.119.187/workspace/ilear/MVC/part4/index.php?page=login');
+        }
+    }
+}
+?>

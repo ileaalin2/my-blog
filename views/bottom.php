2119bf00941ea34c46104ec727fb7c639fca8640
ileaalin2 <ilea.alin2@gmail.com>
2015-09-09 13:18:36 +0300

    Create bottom.php

---
+++ views/bottom.php
@@ -0,0 +1,24 @@
+            <div class="footer">
+                <div class="col-12">
+                    <p>My Team from Informal School of IT:</p>
+                    <ul>
+                        <li><a href="http://188.166.119.187/workspace/fatig/curs1/tema/index.html" target="_blank">Fati Romeo</a></li>
+                        <li><a href="http://188.166.119.187/workspace/ilear/curs1/tema/index.html" target="_blank">Ilea Alin</a></li>
+                        <li><a href="http://188.166.119.187/workspace/ciosanp/curs1/tema/index.html" target="_blank">Ciosan Paul</a></li>
+                        <li><a href="http://188.166.119.187/workspace/mocans/curs1/tema/index.html" target="_blank">Mocan Daniel</a></li>
+                        <li><a href="http://188.166.119.187//workspace/hacicua/curs1/tema/index.html" target="_blank">Hacicu Alex</a></li>
+                        <li><a href="http://188.166.119.187/workspace/lazurcaa/curs1/tema/index.html" target="_blank">Lazurca Andrei</a></li>
+                        <li><a href="http://188.166.119.187/workspace/echertr/curs1/tema/index.html" target="_blank">Echert Robert</a></li>
+                        <li><a href="http://188.166.119.187/workspace/hadarigd/curs1/tema/index.html" target="_blank">Hadarig Dan</a></li>
+                        <li><a href="http://188.166.119.187/workspace/hanganut/curs1/tema/index.html" target="_blank">Hanganu Teodora</a></li>
+                        <li><a href="http://188.166.119.187//workspace/prodans/curs1/tema/index.html" target="_blank">Prodan Sergiu</a></li>
+                        <li><a href="http://188.166.119.187/workspace/girdanr/curs1/tema/index.html" target="_blank">Girdan Roxana</a></li>
+                        <li><a href="http://188.166.119.187/workspace/tanasea/curs1/tema/index.html" target="_blank">Tanase Andrei</a></li>
+                        <li><a href="http://188.166.119.187/workspace/simonv/curs1/tema/index.html" target="_blank">Simon Daniel</a></li>
+                        <li><a href="http://188.166.119.187/workspace/vomveab/curs1/tema/index.html" target="_blank">Bogdan Vomvea</a></li>
+                    </ul>
+                    <p>&#169; 2015, Alin Ilea</p>
+            	</div>
+            </div>
+	</body>
+</html>

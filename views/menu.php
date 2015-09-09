1b418867c3dc63a194304321a52bb18bc9b53f0d
ileaalin2 <ilea.alin2@gmail.com>
2015-09-09 13:16:55 +0300

    Create menu.php

---
+++ views/menu.php
@@ -0,0 +1,18 @@
+<nav class="row">
+    <div class="col-6">
+        <ul class="nav_bar">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="index.php?page=hobby">Hobbies</a></li>
+            <li><a href="index.php?page=language">Languages</a></li>
+            <li><a href="index.php?page=contact">Contact</a></li>
+        </ul>
+    </div>
+    <div class="col-6">
+        <ul class="nav_bar nav_admin">
+            <li>{{logged}}</li>
+            <li><a href="index.php?page=signup">Sign up</a></li>
+            <li><a href="index.php?page=login">Login</a></li>
+            <li><a href="index.php?page=admin">Admin</a></li>
+        </ul>
+    </div>
+</nav>
